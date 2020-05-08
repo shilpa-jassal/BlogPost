ffd5b8ec246adc20cd11737c679c32be93170def
shilpa <shilpa.zapbuild@gail.com>
2020-05-08 12:00:51 +0530

    my second comment

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,12 @@
  
 
 from . import views
-from django.urls import path
-
+from django.urls import path,include
+from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
-  
+    path('contact/',views.contact_form,name='contact'),
+    path('signup/',views.signup,name='signup')
+     
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Post
 
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login
+from .forms import UserCreateForm
+
  
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
- 
     template_name = 'blog/index.html'  # a list of all posts will be displayed on index.html
 
 
@@ -14,5 +21,52 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'  # detail about each blog post will be on post_detail.html
 
+def indexView(request):
+    return render(request,'index.html')
 
- 
+def registerView(request):
+    if request.method=='POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form=UserCreationForm()
+
+    return render(request,'registeration/register.html',{'form':form})
+
+
+
+
+
+def contact_form(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = f'Message from {form.cleaned_data["name"]}'
+            message = form.cleaned_data["message"]
+            sender = form.cleaned_data["email"]
+            recipients = ['thekodechamp@gmail.com']
+            try:
+                send_mail(subject, message, sender, recipients, fail_silently=True)
+            except BadHeaderError:
+                return HttpResponse('Invalid header found')
+            return HttpResponse('Success...Your email has been sent')
+    return render(request, 'blog/contact.html', {'form': form})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+            login(request, new_user)
+            return redirect('home')
+    else:
+        form = UserCreateForm()
+    return render(request, 'registration/signup.html', {'form': form})
